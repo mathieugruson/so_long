2884b623ec0a16101fac80447443a4a32d1e6ad0
Mathieu GRUSON <mathieu.gruson@gmail.com>
2022-10-18 16:21:30 +0200

    error corrected

--- get_map_size.c
+++ get_map_size.c
@@ -6,7 +6,7 @@
 /*   By: mgruson <mgruson@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/17 14:42:18 by mgruson           #+#    #+#             */
-/*   Updated: 2022/10/18 16:11:15 by mgruson          ###   ########.fr       */
+/*   Updated: 2022/10/18 16:20:38 by mgruson          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,7 +46,7 @@ t_info	get_map_tab_size(char **tab)
 
 	map.x = 0;
 	map.y = 0;
-	map.x = ft_strlen(tab[0]);
+	map.x = ft_strlen(tab[0]) - 1;
 	i = 0;
 	while (tab[i])
 	{
--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: mgruson <mgruson@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/16 18:22:38 by mgruson           #+#    #+#             */
-/*   Updated: 2022/10/18 16:07:03 by mgruson          ###   ########.fr       */
+/*   Updated: 2022/10/18 16:13:37 by mgruson          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,14 +17,11 @@ int	main()
 	char **map;	
 	map = NULL;
 	map = get_map("map.ber");
-	ft_puttab(map);
-	ft_free_tab(map);	
-	
-	// if (map_error(map))
-	// {
-	// 	ft_printf("Error\n");
-	// 	return (0);	
-	// }
-	// printf("No error\n");
+	if (map_error(map))
+	{
+		ft_printf("Error\n");
+		return (0);	
+	}
+	printf("No error\n");
 	return (0);
 }
