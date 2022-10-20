cf38d9f200f77704a72fae2b2df02836a5a98fb3
Mathieu GRUSON <mathieu.gruson@gmail.com>
2022-10-20 17:03:44 +0200

    display_map

--- main.c
+++ main.c
@@ -6,174 +6,100 @@
 /*   By: mgruson <mgruson@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/16 18:22:38 by mgruson           #+#    #+#             */
-/*   Updated: 2022/10/19 22:37:03 by mgruson          ###   ########.fr       */
+/*   Updated: 2022/10/20 16:36:03 by mgruson          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
 
-// int	main(int argc, char **argv)
-// {
-// 	char	**map;	
-// 	void	*mlx;
-// 	void	*mlx_win;
+#define IMG_DIMENSION 34
 
-// 	if (argc != 2)
-// 		return (0);
-// 	map = NULL;
-// 	map = get_map(argv[1]);
-// 	if (map_error(map))
-// 	{
-// 		ft_printf("Error\n");
-// 		return (0);	
-// 	}
-// }
-
-#include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
-#include <X11/X.h>
-#include <X11/keysym.h>
-
-#define WINDOW_WIDTH 600
-#define WINDOW_HEIGHT 300
-
-#define MLX_ERROR 1
-
-#define RED_PIXEL 0xFF0000
-#define GREEN_PIXEL 0xFF00
-#define WHITE_PIXEL 0xFFFFFF
-
-typedef struct s_img
+typedef struct img_ptr
 {
-	void	*mlx_img;
-	char	*addr;
-	int		bpp; /* bits per pixel */
-	int		line_len;
-	int		endian;
-}	t_img;
+	void	*all;
+	void	*zero;
+	void	*p;
+	void	*e;
+	void	*c;	
+}	t_img_ptr;
 
-typedef struct s_data
+typedef struct mlx
 {
 	void	*mlx_ptr;
 	void	*win_ptr;
-	t_img	img;
-	int		cur_img;
-}	t_data;
+	t_img_ptr	img_ptr;
+	t_img_ptr	img_relative_path;	
+}	t_mlx;
 
-typedef struct s_rect
+t_info	get_window_size(t_info map_size, int img_dimension)
 {
-	int	x;
-	int	y;
-	int width;
-	int height;
-	int color;
-}	t_rect;
-
-void	img_pix_put(t_img *img, int x, int y, int color)
-{
-	char    *pixel;
-	int		i;
-
-	i = img->bpp - 8;
-    pixel = img->addr + (y * img->line_len + x * (img->bpp / 8));
-	while (i >= 0)
-	{
-		/* big endian, MSB is the leftmost bit */
-		if (img->endian != 0)
-			*pixel++ = (color >> i) & 0xFF;
-		/* little endian, LSB is the leftmost bit */
-		else
-			*pixel++ = (color >> (img->bpp - 8 - i)) & 0xFF;
-		i -= 8;
-	}
-}
-
-/* The x and y coordinates of the rect corresponds to its upper left corner. */
-
-int render_rect(t_img *img, t_rect rect)
-{
-	int	i;
-	int j;
-
-	i = rect.y;
-	while (i < rect.y + rect.height)
-	{
-		j = rect.x;
-		while (j < rect.x + rect.width)
-			img_pix_put(img, j++, i, rect.color);
-		++i;
-	}
-	return (0);
-}
-
-void	render_background(t_img *img, int color)
-{
-	int	i;
-	int	j;
-
-	i = 0;
-	while (i < WINDOW_HEIGHT)
-	{
-		j = 0;
-		while (j < WINDOW_WIDTH)
-		{
-			img_pix_put(img, j++, i, color);
-		}
-		++i;
-	}
-}
-
-int	handle_keypress(int keysym, t_data *data)
-{
-	if (keysym == XK_Escape)
-	{
-		mlx_destroy_window(data->mlx_ptr, data->win_ptr);
-		data->win_ptr = NULL;
-	}
-	return (0);
-}
-
-int	render(t_data *data)
-{
-	if (data->win_ptr == NULL)
-		return (1);
-	render_background(&data->img, WHITE_PIXEL);
-	render_rect(&data->img, (t_rect){WINDOW_WIDTH - 100, WINDOW_HEIGHT - 100, 100, 100, GREEN_PIXEL});
-	render_rect(&data->img, (t_rect){0, 0, 100, 100, RED_PIXEL});
-
-	mlx_put_image_to_window(data->mlx_ptr, data->win_ptr, data->img.mlx_img, 0, 0);
-
-	return (0);
-}
-
-int	main(void)
-{
-	t_data	data;
-
-	data.mlx_ptr = mlx_init();
-	if (data.mlx_ptr == NULL)
-		return (MLX_ERROR);
-	data.win_ptr = mlx_new_window(data.mlx_ptr, WINDOW_WIDTH, WINDOW_HEIGHT, "my window");
-	if (data.win_ptr == NULL)
-	{
-		free(data.win_ptr);
-		return (MLX_ERROR);
-	}
-
-	/* Setup hooks */ 
-	data.img.mlx_img = mlx_new_image(data.mlx_ptr, WINDOW_WIDTH, WINDOW_HEIGHT);
+	t_info	window_size;
 	
-	data.img.addr = mlx_get_data_addr(data.img.mlx_img, &data.img.bpp,
-			&data.img.line_len, &data.img.endian);
+	window_size.x = img_dimension * map_size.x;
+	window_size.y = img_dimension * map_size.y; 
+	
+	return (window_size);
+}
 
-	mlx_loop_hook(data.mlx_ptr, &render, &data);
-	mlx_hook(data.win_ptr, KeyPress, KeyPressMask, &handle_keypress, &data);
+void	display_at_window(char **map, t_info window_size)
+{
+	int		y;
+	int		x;
+	t_mlx	mlx;
+	t_info	img_size;
+	t_info	element_coordinates;
 
-	mlx_loop(data.mlx_ptr);
+	y = 0;
+	x = 0;
+	img_size.x = IMG_DIMENSION;
+	img_size.y = IMG_DIMENSION;
 
-	/* we will exit the loop if there's no window left, and execute this code */
-	mlx_destroy_image(data.mlx_ptr, data.img.mlx_img);
-	mlx_destroy_display(data.mlx_ptr);
-	free(data.mlx_ptr);
+	mlx.mlx_ptr = mlx_init();	
+	mlx.img_relative_path.all = "./img/grass.xpm";
+	mlx.img_relative_path.p = "./img/character.xpm";
+	
+	mlx.win_ptr = mlx_new_window(mlx.mlx_ptr, window_size.x, window_size.y, "Hello world!");
+	mlx.img_ptr.all = mlx_xpm_file_to_image(mlx.mlx_ptr, mlx.img_relative_path.all, &img_size.x, &img_size.y);
+	mlx.img_ptr.p = mlx_xpm_file_to_image(mlx.mlx_ptr, mlx.img_relative_path.p, &img_size.x, &img_size.y);
+
+	while (map[y])
+	{
+		x = 0;
+		while (map[y][x])
+		{
+			if (map[y][x] == '0' || map[y][x] == 'P' )
+				mlx_put_image_to_window(mlx.mlx_ptr, mlx.win_ptr, mlx.img_ptr.all, (IMG_DIMENSION * x), (IMG_DIMENSION * y));
+			if (map[y][x] == 'P')
+				mlx_put_image_to_window(mlx.mlx_ptr, mlx.win_ptr, mlx.img_ptr.p, (IMG_DIMENSION * x), (IMG_DIMENSION * y));
+			x++;
+		}
+		y++;
+	}
+	mlx_loop(mlx.mlx_ptr);	
+}
+
+
+int	main(int argc, char **argv)
+{
+	char	**map;
+	void	*mlx_ptr;
+	void	*win_ptr;
+	void	*img_ptr;
+	char	*img_relative_path;
+	t_info	map_size;
+	t_info	window_size;
+	t_info	img_size;
+
+	if (argc != 2)
+		return (0);
+	map = NULL;
+	map = get_map(argv[1]);
+	map_size = get_map_tab_size(map);
+	window_size = get_window_size(map_size, IMG_DIMENSION);
+	if (map_error(map))
+	{
+		ft_printf("Error\n");
+		return (0);	
+	}
+	display_at_window(map, window_size);
 }
